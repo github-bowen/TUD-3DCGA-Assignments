f6976bfe129cabaa5d2472bb25827944f16f61ad
Bowen <B.Lu-2@student.tudelft.nl>
2024-10-02 02:23:53 +0200

    finished q4 exercise 4

--- Exercise Set 04 - Shadow Mapping/shaders/shader_frag.glsl
+++ Exercise Set 04 - Shadow Mapping/shaders/shader_frag.glsl
@@ -16,6 +16,8 @@ uniform int lightColorMode = 0;
 
 // Bias for shadow comparison to avoid self-shadowing
 const float bias = 0.0005;
+// Spotlight dimming power
+const float spotlightPower = 0.5;  // 1: smooth boundary, 0.1: sharp boundary
 
 
 // Output for on-screen color.
@@ -25,6 +27,22 @@ out vec4 outColor;
 in vec3 fragPos; // World-space position
 in vec3 fragNormal; // World-space normal
 
+
+// calculate spotlight dimming based on distance from shadow map center
+float spotlightDimming(vec2 shadowMapCoord) {
+    
+    float distanceFromCenter = distance(shadowMapCoord, vec2(0.5));
+
+    // If the distance is greater than 0.5, the light should be fully dimmed (outside spotlight)
+    if (distanceFromCenter >= 0.5) return 0.0;
+
+    // Otherwise, calculate the dimming factor, scaling the distance and raising to the power
+    float dimmingFactor = pow(1.0 - (distanceFromCenter / 0.5), spotlightPower);
+    return dimmingFactor;
+}
+
+
+// calculate the shadow factor
 float shadowTest() {
     // Transform the fragment position to light space
     vec4 fragLightCoord = lightMVP * vec4(fragPos, 1.0);
@@ -41,23 +59,27 @@ float shadowTest() {
     // Shadow map coordinate corresponding to this fragment
     vec2 shadowMapCoord = fragLightCoord.xy;
 
+    // Handle spotlight dimming based on distance from shadow map center
+    float dimmingFactor = spotlightDimming(shadowMapCoord);
+
     if (samplingMode == 0) {
         // Shadow map value from the corresponding shadow map position
         float shadowMapDepth = texture(texShadow, shadowMapCoord).x;
         // if the fragment's depth is greater than the shadow map depth (plus bias), it's in shadow
-        return currentDepth - bias > shadowMapDepth ? 0 : 1.0;
+        float shadowFactor = currentDepth - bias > shadowMapDepth ? 0 : 1.0;
+        return shadowFactor * dimmingFactor;
     } else {  // PCF
-        float shadow = 0.0;
+        float shadowFactor = 0.0;
         vec2 texelSize = 1.0 / textureSize(texShadow, 0);
         for(int x = -2; x <= 2; ++x) {
             for(int y = -2; y <= 2; ++y) {
                 vec2 offset = vec2(x, y) * texelSize;
                 float sampleShadowMapDepth = texture(texShadow, shadowMapCoord + offset).x; 
-                shadow += currentDepth - bias > sampleShadowMapDepth ? 0.0 : 1.0;        
+                shadowFactor += currentDepth - bias > sampleShadowMapDepth ? 0.0 : 1.0;        
             }    
         }
-        shadow /= 25.0;  // number of samples: 25
-        return shadow;
+        shadowFactor /= 25.0;  // number of samples: 25
+        return shadowFactor * dimmingFactor;
     }
 }
 
