429319173bb64d7a68ede5d0d8b2a09201ddb8e9
Bowen <B.Lu-2@student.tudelft.nl>
2024-10-25 18:03:38 +0200

    update clear.py

--- clear.py
+++ clear.py
@@ -9,12 +9,13 @@ init(autoreset=True)
 def clear_folders(base_dir):
     # Walk through the directory
     has_error = False
+    executed = False
     for root, dirs, files in os.walk(base_dir):
         for dir_name in dirs:
             # Check for 'out' and '.vs' folders
             if dir_name == 'out' or dir_name == '.vs' or dir_name == '.vscode':
                 folder_path = os.path.join(root, dir_name)
-                
+                executed = True
                 try:
                     shutil.rmtree(folder_path)  # Remove the folder and all its contents
                     print(f"{Fore.GREEN}[Success] Removing folder: {folder_path}")
@@ -23,14 +24,16 @@ def clear_folders(base_dir):
                     print(f"{Fore.RED}[Error]   Removing folder: {folder_path} ------ ", end="")
                     print(f"{Fore.RED}Details: {e}")
 
-    return not has_error
+    return (not has_error, executed)
 
 if __name__ == "__main__":
     # base_directory = input("Enter the base directory to clear 'out/' '.vscode' and '.vs/' folders: ")
     base_directory = "./"
-    success = clear_folders(base_directory)
+    success, executed = clear_folders(base_directory)
     print()
-    if success:
+    if success and executed:
         print(f"{Fore.GREEN}Clearing complete. All out/ .vscode/ and .vs/ folders have been removed.")
+    elif success and not executed:
+        print(f"{Fore.YELLOW}Clearing complete. No out/ .vscode/ or .vs/ folders found.")
     else:
         print(f"{Fore.YELLOW}Clearing not complete. Some folders could not be removed.")
